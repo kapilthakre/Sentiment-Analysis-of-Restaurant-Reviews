84036d21235e012bcc6b3cff6a7d5bfc113f1ecf
kapilthakre <57772985+kapilthakre@users.noreply.github.com>
2020-07-01 18:42:44 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle
-import nltk
-import re
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
 
 st.title("Restaurant Review's Sentiment Analyser")
 st.markdown("*A Machine Learning Web App, Built with Streamlit, Deployed using Heroku*")
