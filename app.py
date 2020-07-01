94a17d0fd88775e63b917e4a328a4303ec3cc09a
kapilthakre <57772985+kapilthakre@users.noreply.github.com>
2020-07-01 20:13:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+import pickle
+import pandas as pd
+#from flasgger import Swagger
+import streamlit as st
+
+def main():
+    st.title("Bank Authenticator")
+
+
+if __name__=='__main__':
+    main()
